ebe91699d77fda83190e18d2b06e22c681280b13
kaznt81 <kaznt@mail.ru>
2023-04-19 15:32:22 +0500

    fix_err

--- tgbot/db.py
+++ tgbot/db.py
@@ -1,18 +1,19 @@
 import oracledb
 import tgbot.config as config
 
-from bot import config, logger
+#from ..bot import logger
 
 def check_conn():
     try:
         config.cursor.execute("select sysdate from dual").fetchone()
         return config.cursor
     except oracledb.DatabaseError or oracledb.OperationalError:
-        logger.info("БД - ошибка связи")
+        print("БД - ошибка связи")
         conn = oracledb.connect(user=config.db.user, password=config.db.password, dsn=config.db.host+config.db.database)
         config.cursor = conn.cursor()
         return config.cursor
     
-async def get_user(t_user):
+def get_user(t_user):
     cursor = check_conn()
     sql = cursor.execute("select * from st_user where u_id=:t_id", t_id=t_user).fetchone()
+    print(sql)
