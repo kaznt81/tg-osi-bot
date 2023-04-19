5b7c9f32f854093ba5e33d077ec03fca1784804a
kaznt81 <kaznt@mail.ru>
2023-04-19 15:21:17 +0500

    add_db

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -7,7 +7,7 @@ import tgbot.db as db
 async def user_start(message: Message):
     if db.get_user(message.from_user.id) is None:
         await message.answer('Сәлеметсіз бе, жұмысты жалғастыру үшін байланыс деректерін бөлісу керек./Здравствуйте, вам нужно поделиться своими контактными данными, чтобы продолжить')
-    await message.reply("Hello, user!")
+        await message.reply("Hello, user!")
 
 
 def register_user(dp: Dispatcher):
